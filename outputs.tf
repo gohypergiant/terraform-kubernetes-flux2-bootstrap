a6269415e84447c0bbdb59ddcb1a1f063ed4eea9
Bren Briggs <code@fraq.io>
2021-04-08 19:51:58 -0400

    Migrate from eks-services

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-// Copyright 2020 Hypergiant, LLC
-
-
+output "github_deploy_key" {
+  description = "The TLS key to add as a deploy key to any private github repos"
+  value       = tls_private_key.main.public_key_openssh
+}
---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 0.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
