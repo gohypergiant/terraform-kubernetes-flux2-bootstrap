b6e0af488db8c46171962a5ec7caf5542fd2f255
Jason Roberts <jr@thrawny.com>
2021-05-12 09:20:50 -0600

    Add optional support for deploying the image automation controller

--- variables.tf
+++ variables.tf
@@ -37,3 +37,9 @@ variable "flux_ssh_known_hosts" {
   default     = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
   description = "SSH known hosts used to access private helm repos via git SSH. See https://github.com/fluxcd/helm-operator/blob/master/chart/helm-operator/README.md#use-a-private-git-server"
 }
+
+variable "flux_deploy_image_automation" {
+  type        = bool
+  default     = false
+  description = "Optionally deploy the image automation controller with the gitops toolkit"
+}
